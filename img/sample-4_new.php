c41b414b7994b95d4ed51c486857821dff488d34
ilayFriedman <ilayfri@bgu.ac.il>
2021-02-10 16:10:11 +0200

    check

--- img/sample-4_new.php
+++ img/sample-4_new.php
@@ -929,7 +929,7 @@ editor.start();
 
     }
   </script>
-<script src="js/jquery.min.js"></script>
+
     <script src="js/vendor/jquery-3.3.1.min.js"></script>
     <script src="js/vendor/bootstrap.bundle.min.js"></script>
     <script src="js/vendor/perfect-scrollbar.min.js"></script>
