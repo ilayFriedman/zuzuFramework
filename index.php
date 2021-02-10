69c64362c011916b7242153c28733588a5207228
ilayFriedman <ilayfri@bgu.ac.il>
2021-02-10 16:12:46 +0200

    after drawlow

--- index.php
+++ index.php
@@ -398,7 +398,6 @@
         </div>
     </main>
     <!-- end header -->
-    <script src="js/drawflow.min.js"></script>
     <script src="js/jquery.min.js"></script>
     <script src="js/vendor/jquery-3.3.1.min.js"></script>
     <script src="js/vendor/jquery.barrating.min.js"></script>
@@ -410,6 +409,7 @@
     <script src="js/dore.script.js"></script>
     <script src="js/scripts.js"></script>
     <script src="js/vendor/nouislider.min.js"></script>
+    <script src="js/drawflow.min.js"></script>
 
     <div class="modal fade modal-right sidebarpopup" id="exampleModalRight" tabindex="-1" role="dialog"
         aria-labelledby="exampleModalRight" aria-hidden="false" data-backdrop="static" data-keyboard="false">
